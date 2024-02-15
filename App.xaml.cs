49228a039326273a86874edf400f91e74827f260
eytuk <eytuk@JI>
2024-02-15 17:08:18 +0100

    Ajoutez des fichiers projet.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Crates
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Crates
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
